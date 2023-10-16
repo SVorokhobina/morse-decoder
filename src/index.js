4f92ba162b261878fe8e5dba8d5573d47d076ea3
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2023-10-16 19:53:23 +1300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let s;
+    let array = [];
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        if (expr[i] == "*") {
+            array.push(" ");
+        } else {
+            s = expr.substr(i, 10);
+            array.push( Morse_to_letter(tenBits_to_Morse(s)) );
+        }
+    }
+    return array.join("");
+    
+    function Morse_to_letter(str) {
+        return MORSE_TABLE[str];
+    }
+    
+    function tenBits_to_Morse(str) {
+        let arr = [];
+        for (let i = 0; i < str.length; i += 2) {
+            if ((str[i] == 1) && (str[i + 1] == 1)) { arr.push("-"); }
+            else if ((str[i] == 1) && (str[i + 1] == 0)) { arr.push("."); }
+            else if (str[i] == 0) { continue; }
+        }
+        return arr.join("");
+    }
 }
 
 module.exports = {
